a59f047421765a629e3fcbc936214e5f03bbea02
NikitaPyat <73129357+NikitaPyat@users.noreply.github.com>
2021-01-31 04:05:13 +0300

    Add files via upload

--- Lab_2_main.cs
+++ Lab_2_main.cs
@@ -15,7 +15,7 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            /*V5DataCollection a = new V5DataCollection("file.txt");
+            V5DataCollection a = new V5DataCollection("file.txt");
             Console.WriteLine(a.ToLongString("f5"));
             V5MainCollection b = new V5MainCollection();
             b.AddDefaults();
@@ -32,7 +32,7 @@ namespace Lab_2
                 Console.WriteLine(s1.ToLongString("f5"));
             Console.WriteLine("DataItems");
             foreach (DataItem s2 in b.Ditems)
-                Console.WriteLine(s2.ToLongString("f5"));*/
+                Console.WriteLine(s2.ToLongString("f5"));
 
 
             V5MainCollection c = new V5MainCollection();
@@ -40,6 +40,7 @@ namespace Lab_2
             c.AddDefaults();
             c[1] = new V5DataCollection("MainCollection", new DateTime()){info = "Rename"};
             c.Remove("Rename", c[1].dateD);
+
         }
         static void Cout(object source, DataChangedEventArgs args)
         {
